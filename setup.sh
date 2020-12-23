88c290b86a898cd1fd2d2910ec924c9b463fa123
Khalid Al-Sahli <alsahli.khaled.n@gmail.com>
2020-12-23 23:24:29 +0300

    Config Files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-# @TODO export database_path='????'
-
-# @TODO export AUTH_DOMAIN='????'
+export DATABASE_URL=''
+export AUTH0_DOMAIN='fsnd-khaled.us.auth0.com'
 export ALGORITHMS=['RS256']
-#@TODO export API_AUDIENCE='??'
+export API_AUDIENCE='fnsd_capstone'
+export CLIENT_ID='LguNv4OPj7tWNe1ov12uZjiOLoQnxJwQ'
